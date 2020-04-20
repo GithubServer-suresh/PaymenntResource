104a1c027e16e2b42e6541d94ff9d83ce5f5f7cc
Suresh Suri <Suresh Suri@SureshSuri>
2020-04-20 19:38:08 +0530

    Added Logger to viewt of TestController

--- src/main/java/com/spring/rest/curd/controller/TestController.java
+++ src/main/java/com/spring/rest/curd/controller/TestController.java
@@ -1,9 +1,14 @@
 package com.spring.rest.curd.controller;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class TestController {
 
+	Logger log= Logger.getLogger("TestController");
 	
 	public String viewt() {
+		log.log(Level.ALL, this.viewt(), "Message");
 		System.out.println("Jenkins 3");
 		return "Vendor";
 	}

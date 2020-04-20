8d358832e99586d369ccadcf942252f0358e55e2
Suresh Suri <Suresh Suri@SureshSuri>
2020-04-20 21:38:15 +0530

    SOP added to TestController

--- src/main/java/com/spring/rest/curd/controller/TestController.java
+++ src/main/java/com/spring/rest/curd/controller/TestController.java
@@ -5,12 +5,11 @@ import java.util.logging.Logger;
 
 public class TestController {
 
-	//Logger log= Logger.getLogger("TestController");
+	Logger log= Logger.getLogger("TestController");
 	
 	public String viewt() {
 		log.log(Level.ALL, this.viewt(), "Message");
-		System.out.println("Jenkins 3");
-		System.out.println("Jenkins 4");
+		System.out.println("SOP added");
 		return "Vendor";
 	}
 	

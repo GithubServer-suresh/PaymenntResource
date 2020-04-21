a35a84a3a6800fca85b010d269686d1a94b639b1
Suresh Suri <Suresh Suri@SureshSuri>
2020-04-21 06:08:53 +0530

    Removed Logger Statements

--- src/main/java/com/spring/rest/curd/controller/TestController.java
+++ src/main/java/com/spring/rest/curd/controller/TestController.java
@@ -1,14 +1,9 @@
 package com.spring.rest.curd.controller;
 
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 public class TestController {
-
-	Logger log= Logger.getLogger("TestController");
 	
 	public String viewt() {
-		log.log(Level.ALL, this.viewt(), "Message");
 		System.out.println("SOP added");
 		return "Vendor";
 	}
